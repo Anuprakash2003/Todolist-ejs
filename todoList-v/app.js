1fd1ae3bc11d2a0c8dc998d2c5ebf7c781e79782
Anuprakash.cs20@bitsathy.ac.in <Anuprakash.cs20@bitsathy.ac.in>
2022-07-20 08:57:10 +0530

    v3

--- todoList-v/app.js
+++ todoList-v/app.js
@@ -1,43 +1,19 @@
 const express = require("express");
-const bodyParser=require("body-parser");
-const ejs=require("ejs");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 
-const app=express();
+const app = express();
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
-app.get("/",function(req,res){
+app.get("/", function(req, res) {
     var today = new Date();
-    var day =today.getDay();
-    var week="";
-   switch(day){
-       case 0:
-         week="Sunday";
-         break;
-       case 1:
-          week="Monday";
-          break;
-        case 2:
-          week="Tuesday";
-          break;
-       case 3:
-          week="Wednesday";
-          break;
-       case 4:
-          week="Thursday";
-          break;   
-       case 5:
-          week="Friday";
-          break;  
-       case 6:
-          week="Saturday";
-          break;  
-   }
-    res.render("list",{kindOfDay:week});
+
+    res.render("list", { kindOfDay: week });
 });
 
-app.listen(4000,function(req,res){
+app.listen(4000, function(req, res) {
 
 
     console.log("server started at port 4000");
