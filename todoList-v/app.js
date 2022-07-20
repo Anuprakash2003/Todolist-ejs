aa930298bc47dc41373d90cd1ab192bc99218846
Anuprakash.cs20@bitsathy.ac.in <Anuprakash.cs20@bitsathy.ac.in>
2022-07-20 14:56:41 +0530

    version-1

--- todoList-v/app.js
+++ todoList-v/app.js
@@ -3,14 +3,33 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
 
+var items = ["cook a food", "buy items", "Eat Food"]
 const app = express();
 
+
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
 
 app.get("/", function(req, res) {
     var today = new Date();
+    var option = {
 
-    res.render("list", { kindOfDay: week });
+        day: "numeric",
+        weekday: "long",
+        month: "long"
+    };
+    var day = today.toLocaleDateString("en-US", option);
+    res.render("list", { kindOfDay: day, newListItems: items })
+
+
+    res.render("list", { kindOfDay: day });
+});
+app.post("/", function(req, res) {
+    var Item = req.body.newItem;
+    items.push(Item);
+    res.redirect("/");
 });
 
 app.listen(4000, function(req, res) {
