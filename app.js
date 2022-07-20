36ec26973c14de552e4fe7dbe25fc624b079e82f
unknown <anuprakash.cs20@bitsathy.ac.in>
2022-07-20 19:26:39 +0530

    master directory

--- app.js
+++ app.js
@@ -1,43 +1,47 @@
 const express = require("express");
-const bodyParser=require("body-parser");
-const ejs=require("ejs");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 
-const app=express();
+var items = ["cook a food", "buy items", "Eat Food"]
+var workItems = [];
+const app = express();
 
-app.set('view engine','ejs');
 
-app.get("/",function(req,res){
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get("/", function(req, res) {
     var today = new Date();
-    var day =today.getDay();
-    var week="";
-   switch(day){
-       case 0:
-         week="Sunday";
-         break;
-       case 1:
-          week="Monday";
-          break;
-        case 2:
-          week="Tuesday";
-          break;
-       case 3:
-          week="Wednesday";
-          break;
-       case 4:
-          week="Thursday";
-          break;   
-       case 5:
-          week="Friday";
-          break;  
-       case 6:
-          week="Saturday";
-          break;  
-   }
-    res.render("list",{kindOfDay:week});
+    var option = {
+
+        day: "numeric",
+        weekday: "long",
+        month: "long"
+    };
+    var day = today.toLocaleDateString("en-US", option);
+    res.render("list", { listTitle: day, newListItems: items })
+
 });
 
-app.listen(4000,function(req,res){
+app.get("/work", function(req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+app.post("/", function(req, res) {
+    var Item = req.body.newItem;
+    if (req.body.list === "work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(Item);
+        res.redirect("/");
+    }
+
+});
+
+app.listen(4000, function(req, res) {
 
 
     console.log("server started at port 4000");
